35d3487e5363adbc3ec206d471d99b9a48dfa2f1
testingperu <testingperu@hotmail.com>
2016-04-17 00:46:06 -0500

    agregando comentario desde CompaqPC 0:45am

--- test2.php
+++ test2.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
-//comentario desde prueba 3
-=======
-//comentario desde prueba 4 PC COMPAQ
->>>>>>> 5e1ddd83d9900444c2dc172487e6468cc3e4aef0
+
 echo "Esto es una prueba con git, Alea jacta est";
-//comment 12:40 am
+//comment 12:45 am
 
 ?>
